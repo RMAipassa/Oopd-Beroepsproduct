71e64f923d759af50f6ab9b442d7bc728ba6ea8b
WubyWuben <r.m.aipassa@student.utwente.nl>
2023-03-17 15:00:49 +0100

    test 2.0

---
+++ DesertGame/src/main/java/nl/desertgame/desert_game/DesertGame.java
@@ -0,0 +1,19 @@
+package nl.desertgame.desert_game;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class DesertGame extends YaegerGame {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
--- OOPD-Beroepsproduct/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module nl.desertgame.desert_game {
-    requires hanyaeger;
-
-    exports nl.desertgame.desert_game;
-
-    opens audio;
-    opens backgrounds;
-    opens sprites;
-}
