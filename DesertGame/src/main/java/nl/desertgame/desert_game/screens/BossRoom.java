c4a976bbe11c7f6738c8bf3ce52736cd227381ff
SemGrienberger <semgrienberger@gmail.com>
2023-03-24 20:45:06 +0100

    Fixed Potions and Changed movement to WASD

--- DesertGame/src/main/java/nl/desertgame/desert_game/screens/BossRoom.java
+++ DesertGame/src/main/java/nl/desertgame/desert_game/screens/BossRoom.java
@@ -5,9 +5,13 @@ import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.api.scenes.TileMapContainer;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import nl.desertgame.desert_game.DesertGame;
 import nl.desertgame.desert_game.entities.Heart;
+import nl.desertgame.desert_game.entities.Image;
 import nl.desertgame.desert_game.entities.Player;
+import nl.desertgame.desert_game.entities.Text;
 import nl.desertgame.desert_game.map.BossMap;
 
 import java.util.ArrayList;
@@ -19,6 +23,7 @@ public class BossRoom extends DynamicScene implements TileMapContainer {
     private DesertGame desertGame;
     private static Player player;
     private static Heart[] hearts;
+    private static Text amountPotion;
 
     public BossRoom(DesertGame desertGame) {
         this.desertGame = desertGame;
@@ -38,6 +43,18 @@ public class BossRoom extends DynamicScene implements TileMapContainer {
         player = new Player(desertGame ,new Coordinate2D(1230, 320));
         addEntity(player);
         setupHearts();
+        setupPotions();
+    }
+    public void setupPotions() {
+        Image potions = new Image("sprites/potion.png", new Coordinate2D(50, 50), new Size(32, 32));
+        addEntity(potions);
+        amountPotion = new Text(new Coordinate2D(80, 52), Player.getPotions() + "x");
+        amountPotion.setFill(Color.WHITE);
+        amountPotion.setFont(Font.font("Roboto", FontWeight.BOLD, 20));
+        addEntity(amountPotion);
+    }
+    public static void updatePotions(){
+        amountPotion.setText(Player.getPotions()+"x");
     }
     public void setupHearts() {
         hearts = new Heart[getTotalHealth()];
@@ -50,13 +67,11 @@ public class BossRoom extends DynamicScene implements TileMapContainer {
     }
     public static void updateHearts() {
         int currentHealth = Player.getHealth();
-        if(hearts.length != currentHealth){
-            for (int i = 0; i < hearts.length; i++) {
-                if (i < currentHealth) {
-                    hearts[i].setCurrentFrameIndex(0); // heart is full
-                } else {
-                    hearts[i].setCurrentFrameIndex(1); // heart is empty
-                }
+        for (int i = 0; i < hearts.length; i++) {
+            if (i < currentHealth) {
+                hearts[i].setCurrentFrameIndex(0); // heart is full
+            } else {
+                hearts[i].setCurrentFrameIndex(1); // heart is empty
             }
         }
     }
--- DesertGame/src/main/java/nl/desertgame/desert_game/screens/EnemyChoiceRoom.java
+++ DesertGame/src/main/java/nl/desertgame/desert_game/screens/EnemyChoiceRoom.java
@@ -5,9 +5,13 @@ import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.api.scenes.TileMapContainer;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import nl.desertgame.desert_game.DesertGame;
 import nl.desertgame.desert_game.entities.Heart;
+import nl.desertgame.desert_game.entities.Image;
 import nl.desertgame.desert_game.entities.Player;
+import nl.desertgame.desert_game.entities.Text;
 import nl.desertgame.desert_game.map.RoomMap;
 
 import static nl.desertgame.desert_game.entities.Player.getTotalHealth;
@@ -18,6 +22,7 @@ public class EnemyChoiceRoom extends DynamicScene implements TileMapContainer {
     private DesertGame desertGame;
     private static Player player;
     private static Heart[] hearts;
+    private  static Text amountPotion;
 
     public EnemyChoiceRoom(DesertGame desertGame) {
         this.desertGame = desertGame;
@@ -37,6 +42,18 @@ public class EnemyChoiceRoom extends DynamicScene implements TileMapContainer {
         player = new Player(desertGame ,new Coordinate2D(50, 320));
         addEntity(player);
         setupHearts();
+        setupPotions();
+    }
+    public void setupPotions() {
+        Image potions = new Image("sprites/potion.png", new Coordinate2D(50, 50), new Size(32, 32));
+        addEntity(potions);
+        this.amountPotion = new Text(new Coordinate2D(80, 52), Player.getPotions() + "x");
+        amountPotion.setFill(Color.WHITE);
+        amountPotion.setFont(Font.font("Roboto", FontWeight.BOLD, 20));
+        addEntity(amountPotion);
+    }
+    public static void updatePotions(){
+       amountPotion.setText(Player.getPotions()+"x");
     }
     public void setupHearts() {
         hearts = new Heart[getTotalHealth()];
@@ -49,13 +66,11 @@ public class EnemyChoiceRoom extends DynamicScene implements TileMapContainer {
     }
     public static void updateHearts() {
         int currentHealth = Player.getHealth();
-        if(hearts.length != currentHealth){
-            for (int i = 0; i < hearts.length; i++) {
-                if (i < currentHealth) {
-                    hearts[i].setCurrentFrameIndex(0); // heart is full
-                } else {
-                    hearts[i].setCurrentFrameIndex(1); // heart is empty
-                }
+        for (int i = 0; i < hearts.length; i++) {
+            if (i < currentHealth) {
+                hearts[i].setCurrentFrameIndex(0); // heart is full
+            } else {
+                hearts[i].setCurrentFrameIndex(1); // heart is empty
             }
         }
     }
